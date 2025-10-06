0902ee157dfa67322cd9ebfa5b54f6b215fe1d67
eugenemantey6-commits <eugenemantey6@gmail.com>
2025-10-06 19:52:23 +0000

    Update settings.js for Abcd

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;XZ0ynQSZ#8rQgnB4ll_TlvFVmFWRilFj8MjQz-h3vnQ83Gk1W20I",
+  OWNER_NUMBER: "+233505675648", 
+  PREFIX: "_",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
